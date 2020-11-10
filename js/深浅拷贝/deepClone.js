e4026d713705fdf799798c2795113516603b248d
francis <2443573565@qq.com>
2020-11-10 19:30:31 +0800

    feat: 日常练习

--- js/深浅拷贝/deepClone.js
+++ js/深浅拷贝/deepClone.js
@@ -1,19 +1,81 @@
-function deepClone(origin){
-  // origin 是null，或者不是对象和数组，直接返回
-  if(typeof origin!=='object' || origin === null){
-    return origin
+// function deepClone(origin){
+//   // origin 是null，或者不是对象和数组，直接返回
+//   if(typeof origin!=='object' || origin === null){
+//     return origin
+//   }
+//   let target
+//   if(Object.prototype.toString.call(origin)==='[object Array]'){
+//     target=[]
+//   }else{
+//     target={}
+//   }
+//   for(let key in origin){
+//     // 保证key不是原型上的属性
+//     if(origin.hasOwnProperty(key)){
+//       // 递归调用
+//       target[key]=deepClone(origin[key])
+//     }
+//   }
+//   return target
+// }
+
+// 第二遍
+// function deepClone(source){
+//   if(typeof source!=='object'||source==null){
+//     return source
+//   }
+//
+//   let target;
+//   if(Object.prototype.toString.call(source)!=='[object Object]'){
+//     target={}
+//   }else if(Object.prototype.toString.call(source)!=='[object Array]'){
+//     target=[]
+//   }
+//
+//   for(let key in source){
+//     if(source.hasOwnProperty(key)){
+//       target[key]=deepClone(source[key])
+//     }
+//   }
+//
+//   return target
+// }
+
+// 第三遍
+// function deepClone(source){
+//   if(typeof source!=='object'|| source==null){
+//     return source
+//   }
+//   let target;
+//   if(Object.prototype.toString.call(source)==='[object Object]'){
+//     target={}
+//   }else if(Object.prototype.toString.call(source)==='[object Array]'){
+//     target=[]	}
+//
+//   for(let key in source){
+//     if(source.hasOwnProperty(key)){
+//       target[key]=deepClone(source[key])
+//     }
+//   }
+//   return target
+//
+// }
+
+// 第四遍
+function deepClone(source){
+  if(typeof source!=='object'||source==null){
+    return source
   }
-  let target
-  if(Object.prototype.toString.call(origin)==='[object Array]'){
-    target=[]
-  }else{
+
+  let target=null;
+  if(Object.prototype.toString.call(source)==='[object Object]'){
     target={}
+  }else if(Object.prototype.toString.call(source)==='[object Array]'){
+    target=[]
   }
-  for(let key in origin){
-    // 保证key不是原型上的属性
-    if(origin.hasOwnProperty(key)){
-      // 递归调用
-      target[key]=deepClone(origin[key])
+  for(let key in source){
+    if(source.hasOwnProperty(key)){
+       target[key]=deepClone(source[key])
     }
   }
   return target

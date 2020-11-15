0983d11e8d71d800387709a89287478286ca2f5a
francis <franciszimo@163.com>
2020-11-15 18:47:45 +0800

    feat: 阶段性练习

---
+++ VUE/vue-project/src/components/lifeCycle/child.vue
@@ -0,0 +1,52 @@
+<template>
+    <div style="border:1px solid red">
+        <h3>子组件</h3>
+        <h3>title:{{title}}</h3>
+        <button @click="change">子组件传参改变父组件</button>
+    </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'child',
+    props:['title'],
+    data(){return{
+      city:'北京'
+    }},
+    methods:{
+      change(){
+        this.$emit('changeData','传给父组件参数')
+      }
+    },
+    beforeCreate () {
+      console.log('子组件 beforeCreate')
+    },
+    created () {
+      console.log('子组件 created')
+    },
+    beforeMount () {
+      console.log(' 子组件 beforeMount')
+    },
+    mounted () {
+      console.log('子组件 mounted')
+    },
+    beforeUpdate(){
+      console.log('子组件 beforeUpdate')
+    },
+    updated () {
+      console.log('子组件 update')
+    },
+    beforeDestroy () {
+      console.log('子组件 beforeDestroy')
+    },
+    destroyed () {
+      console.log('子组件 destroyed')
+    }
+  }
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ VUE/vue-project/src/components/lifeCycle/parent.vue
@@ -0,0 +1,58 @@
+<template>
+    <div style="border:1px solid blue">
+        <h3>生命周期，挂载，更新，与销毁</h3>
+        <h3>父组件</h3>
+        <child :title="name" @changeData="getParams"></child>
+        <button @click="updateCmp">更新父组件</button>
+    </div>
+
+</template>
+
+<script>
+    import child from './child'
+  export default {
+    name: 'parent',
+    data(){
+      return{
+        name:'francis'
+      }
+    },
+    components: {child},
+    methods:{
+      updateCmp(){
+        this.name='传递给父组件的值'
+      },
+      getParams(payload){
+        this.name=payload
+      }
+    },
+    beforeCreate () {
+      console.log('父组件 beforeCreate')
+    },
+    created () {
+      console.log('父组件 created')
+    },
+    beforeMount () {
+      console.log(' 父组件 beforeMount')
+    },
+    mounted () {
+      console.log('父组件 mounted')
+    },
+    beforeUpdate(){
+      console.log('父组件 beforeUpdate')
+    },
+    updated () {
+      console.log('父组件 update')
+    },
+    beforeDestroy () {
+      console.log('父组件 beforeDestroy')
+    },
+    destroyed () {
+      console.log('父组件 destroyed')
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>

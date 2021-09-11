b28d44818431baf140b149945cc15a9d1243453c
wangyongju <wangyongju@tal.com>
2021-09-11 13:55:31 +0800

    feat: 日常练习

--- js/深浅拷贝/shallowClone.js
+++ js/深浅拷贝/shallowClone.js
@@ -1,16 +1,31 @@
+// function shallowClone(origin){
+//   if(typeof origin!=='object'|| origin === null){
+//     return origin
+//   }
+//   let target
+//   if(Object.prototype.toString.call(origin)==='[object Array]'){
+//     target=[]
+//   }else{
+//     target={}
+//   }
+//   for(let key in origin){
+//     target[key]=origin[key]
+//   }
+//   return target
+// }
+
 function shallowClone(origin){
-  if(typeof origin!=='object'|| origin === null){
-    return origin
+  if(typeof origin !=='object' || !origin){
+    return origin 
   }
-  let target
+  let result=null;
   if(Object.prototype.toString.call(origin)==='[object Array]'){
-    target=[]
+    result=[]
   }else{
-    target={}
+    result={}
   }
-  for(let key in origin){
-    target[key]=origin[key]
+  for(let i in origin){
+    result[i]=origin[i]
   }
-  return target
+  return result
 }
-console.log('exec')

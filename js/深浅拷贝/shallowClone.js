6dc2f2031fe123e55e92e2960f76aebf6f2d9034
wangyongju <wangyongju@tal.com>
2021-09-15 10:50:22 +0800

    feat: 21/9/15阶段性练习

--- js/深浅拷贝/shallowClone.js
+++ js/深浅拷贝/shallowClone.js
@@ -14,18 +14,34 @@
 //   return target
 // }
 
+// function shallowClone(origin){
+//   if(typeof origin !=='object' || !origin){
+//     return origin 
+//   }
+//   let result=null;
+//   if(Object.prototype.toString.call(origin)==='[object Array]'){
+//     result=[]
+//   }else{
+//     result={}
+//   }
+//   for(let i in origin){
+//     result[i]=origin[i]
+//   }
+//   return result
+// }
+
 function shallowClone(origin){
-  if(typeof origin !=='object' || !origin){
-    return origin 
+  if(typeof origin !=='origin' || !origin){
+    return origin
   }
-  let result=null;
+  let result={}
   if(Object.prototype.toString.call(origin)==='[object Array]'){
     result=[]
-  }else{
-    result={}
   }
   for(let i in origin){
-    result[i]=origin[i]
+    if(origin.hasOwnProperty(i)){
+      result[i]=origin[i]
+    }
   }
   return result
 }

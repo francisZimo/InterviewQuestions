874daf76a957d71fedf67669f87928877977d976
wangyongju <wangyongju@tal.com>
2022-02-25 17:05:32 +0800

    feat: 阶段练习

---
+++ testNpm/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div></div>
+</template>
+
+<script lang='ts'>
+import { defineComponent, reactive,toRefs} from 'vue'
+import {lottie169Adaptation} from '@xes/xueyan-tools'
+interface DataProps {}
+export default defineComponent( {
+    name: '',
+      setup() {
+          const data: DataProps = reactive({
+
+          })
+          const refData = toRefs(data);
+          return {
+              ...refData,
+          }
+
+      }
+  });
+</script>
+<style scoped lang='scss'>
+</style>

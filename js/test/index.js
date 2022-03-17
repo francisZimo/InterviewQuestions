2c6fdcf58245b883905d85995c06ed04b92e3fc5
wangyongju <wangyongju@tal.com>
2022-03-17 18:44:06 +0800

    feat: 单例测试

---
+++ js/test/index.js
@@ -0,0 +1,30 @@
+/**
+ * @typedef Animal 动物
+ * @property {string} name 名称
+ * @property {string} [live] 名称
+ * @property {number | string} foot 脚
+ */
+
+/**
+ * 动物列表
+ * @type {Animal[]}
+ */
+ const animalList = [
+    {
+      name: 'cat',
+      live: 'land',
+      foot: 4
+    },
+    {
+      name: 'fish',
+      live: 'water',
+      foot: 0
+    },
+    {
+      name: 'dragon',
+      foot: 'some'
+    }
+  ]
+  
+  // animalList[0].live
+  

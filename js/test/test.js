4f099230a2750adf831acde8dd3486f06d413208
wangyongju <wangyongju@tal.com>
2022-09-05 17:40:32 +0800

    feat: 链表22.9.5

---
+++ js/test/test.js
@@ -0,0 +1,17 @@
+let s={
+    self:{
+        avatar: "https://xesfile.xesimg.com/teacher/2020/09/02/15990294621249.jpg",
+        name: "樱桃小丸子樱桃小丸子樱桃小丸子樱桃小丸子"  
+    },
+    opponent:{
+        avatar: "https://xesfile.xesimg.com/teacher/2020/09/02/15990294621249.jpg",
+        name: "柯南123柯南123柯南123柯南123柯南123"
+    },
+    reason: "黑方认输，白方胜！",
+    result: "defeat",
+    pkHistory:{
+        draw: 999,
+        loss: 9999,
+        win: 999
+    }
+}

4af5897106058e323a10944ec1d9907ad4469aa8
wangyongju <wangyongju@tal.com>
2023-05-10 10:07:53 +0800

    feat: 括号匹配

--- 100win/algorithm/括号匹配/isValid.test.js
+++ 100win/algorithm/括号匹配/isValid.test.js
@@ -1,19 +1,25 @@
-import isValid from './isValid'
+// import isValid from './isValid'
+import isValid from './isValid-23yead'
 
-describe('括号匹配',()=>{
-    it('匹配',()=>{
-        const str= "()[]{}"
-        const value=isValid(str)
+describe('括号匹配', () => {
+    it('匹配1', () => {
+        const str = "()"
+        const value = isValid(str)
         expect(value).toBe(true)
     })
-    it('不匹配1',()=>{
-        const str= "(}"
-        const value=isValid(str)
+    it('匹配', () => {
+        const str = "()[]{}"
+        const value = isValid(str)
+        expect(value).toBe(true)
+    })
+    it('不匹配1', () => {
+        const str = "(}"
+        const value = isValid(str)
         expect(value).toBe(false)
     })
-    it('不匹配2',()=>{
-        const str= "}"
-        const value=isValid(str)
+    it('不匹配2', () => {
+        const str = "}"
+        const value = isValid(str)
         expect(value).toBe(false)
     })
 })

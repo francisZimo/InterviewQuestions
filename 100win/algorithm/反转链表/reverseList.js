d915d8798b62e4a8a10da0a47920fd56aebe2821
wangyongju <wangyongju@tal.com>
2023-05-24 18:38:51 +0800

    feat: 阶段练习

--- 100win/algorithm/反转链表/reverseList.js
+++ 100win/algorithm/反转链表/reverseList.js
@@ -6,64 +6,77 @@
  * }
  */
 
- function ListNode(val) {
-        //  this.val = val;
-        // this.next = null;
-        return{
-            val:val,
-            next:null
-        }
-     }
+function ListNode(val) {
+    //  this.val = val;
+    // this.next = null;
+    return {
+        val: val,
+        next: null
+    }
+}
 /**
  * @param {ListNode} head
  * @return {ListNode}
  */
-  export const reverseList=function (head) {
-    let pre=null
-    while(head){
-        const next=head.next
-        head.next=pre
-        pre=head
-        head=next
-    }
-    return pre
-};
+//   export const reverseList=function (head) {
+//     let pre=null
+//     while(head){
+//         const next=head.next
+//         head.next=pre
+//         pre=head
+//         head=next
+//     }
+//     return pre
+// };
 
-const reverseList1=function(head){
-    let pre=null
-    while(head){
-        const next=head.next
-        head.next=pre
-        pre=head
-        head=next
+const reverseList1 = function (head) {
+    let pre = null
+    while (head) {
+        const next = head.next
+        head.next = pre
+        pre = head
+        head = next
     }
     return pre
 }
 
-export const createListNode= function (arr){
-    if(!arr||!arr.length) return null
+export const createListNode = function (arr) {
+    if (!arr || !arr.length) return null
     // const head=new ListNode(0)
-    const head= ListNode(0)
-    let tempNode=head
-    for(let i=0;i<arr.length;i++){
+    const head = ListNode(0)
+    let tempNode = head
+    for (let i = 0; i < arr.length; i++) {
         // const newNode=new ListNode(arr[i])
-        const newNode= ListNode(arr[i])
-        tempNode.next=newNode
-        tempNode=newNode
+        const newNode = ListNode(arr[i])
+        tempNode.next = newNode
+        tempNode = newNode
     }
     return head.next
-   
+
 }
-const createListNode1=function(arr){
-    if(!arr||!arr.length) return null
-    const head=ListNode(0)
-    let tempNode=head
-    for(let i=0;i<arr.length;i++){
-        const item= ListNode(arr[i])
-        tempNode.next=item
-        tempNode=item
+const createListNode1 = function (arr) {
+    if (!arr || !arr.length) return null
+    const head = ListNode(0)
+    let tempNode = head
+    for (let i = 0; i < arr.length; i++) {
+        const item = ListNode(arr[i])
+        tempNode.next = item
+        tempNode = item
     }
     return head.next
 }
-// let s=[100,200,300]
-// console.log(createListNode(s))
+
+export const reverseList = (listNode) => {
+    console.log(listNode, '==null?')
+    if (!listNode) {
+        return null
+    }
+    let tempNode = null
+    while (listNode) {
+        const next = listNode.next
+        listNode.next = tempNode
+        tempNode = listNode
+        listNode = next
+    }
+    return tempNode
+}

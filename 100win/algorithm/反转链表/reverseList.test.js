cc82dd255b67d9b74cfc2051bafb241f37705907
wangyongju <wangyongju@tal.com>
2023-05-24 18:38:51 +0800

    feat: 阶段练习

--- 100win/algorithm/反转链表/reverseList.test.js
+++ 100win/algorithm/反转链表/reverseList.test.js
@@ -1,20 +1,23 @@
-import { isVoidTag } from "@vue/shared"
-import { createListNode ,reverseList} from "./reverseList"
+import {
+    createListNode,
+    reverseList
+} from "./reverseList"
 
-describe('反转链表',()=>{
-   it('反转1',()=>{
-       const arr=[1,2,3]
-       const listNode=createListNode(arr)
-       const res=reverseList(listNode)
-       expect(res).toEqual({
-           val:3,
-           next:{
-               val:2,
-                next:{
-                    val:1,
-                    next:null
+describe('反转链表', () => {
+    it('反转1', () => {
+        const arr = [1, 2, 3]
+        const listNode = createListNode(arr)
+        const res = reverseList(listNode)
+        console.log(res, '===res')
+        expect(res).toEqual({
+            val: 3,
+            next: {
+                val: 2,
+                next: {
+                    val: 1,
+                    next: null
                 }
-           }
-       })
-   })
+            }
+        })
+    })
 })

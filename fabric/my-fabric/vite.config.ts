593527bf36a581ece5dac6560e832bfc85645a2c
wangyongju <wangyongju@tal.com>
2023-06-25 09:56:13 +0800

    feat: 日常更新

--- fabric/my-fabric/vite.config.ts
+++ fabric/my-fabric/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// 本地服务器配置
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+    open: true,
+    host: true,
+  },
   plugins: [vue()],
-})
+});

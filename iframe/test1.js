e71d93cb7cbbbe471dd78b1bc57c3de33c320676
franciszimo <wangyongju@tal.com>
2024-09-18 10:09:47 +0800

    feat: 暗水印

---
+++ iframe/test1.js
@@ -0,0 +1,236 @@
+let s =
+{
+    "type": "formData",
+    "data": {
+        "clientConfig": {
+            "studentHotArea": {
+                "count": 6,
+                "operationType": "All",
+                "location": [
+                    {
+                        "id": "f4389c0b-cb45-4d3f-a40d-8722f62147c0",
+                        "index": 1,
+                        "name": "学生热区_1",
+                        "hotAreaType": "student",
+                        "interactionType": "videoAudio",
+                        "role": "-1",
+                        "aiSticker": "-1"
+                    },
+                    {
+                        "id": "23fcc53e-6a94-456d-ae19-839501f24a2b",
+                        "index": 2,
+                        "name": "学生热区_2",
+                        "hotAreaType": "student",
+                        "interactionType": "videoAudio",
+                        "role": "-1",
+                        "aiSticker": "-1"
+                    },
+                    {
+                        "id": "094c9d88-a46a-4b75-aefb-5a574a9fc786",
+                        "index": 3,
+                        "name": "学生热区_3",
+                        "hotAreaType": "student",
+                        "interactionType": "videoAudio",
+                        "role": "-1",
+                        "aiSticker": "-1"
+                    },
+                    {
+                        "id": "12b2449d-72ae-4938-8de9-7cb9b912cf8f",
+                        "index": 4,
+                        "name": "学生热区_4",
+                        "hotAreaType": "student",
+                        "interactionType": "videoAudio",
+                        "role": "-1",
+                        "aiSticker": "-1"
+                    },
+                    {
+                        "id": "ac482442-3f12-40b4-b28d-b76a30f34303",
+                        "index": 5,
+                        "name": "学生热区_5",
+                        "hotAreaType": "student",
+                        "interactionType": "videoAudio",
+                        "role": "-1",
+                        "aiSticker": "-1"
+                    },
+                    {
+                        "id": "10041042-967e-4151-bf78-6240e597c9f3",
+                        "index": 6,
+                        "name": "学生热区_6",
+                        "hotAreaType": "student",
+                        "interactionType": "videoAudio",
+                        "role": "-1",
+                        "aiSticker": "-1"
+                    }
+                ]
+            },
+            "partName": "",
+            "stageHotArea": {
+                "count": 1,
+                "interactionCommand": "designatedOnStage",
+                "operationType": "All",
+                "location": [
+                    {
+                        "id": "8510659f-a677-4bc6-9d90-aa473e64b7a3",
+                        "index": 1,
+                        "name": "上台热区_1",
+                        "hotAreaType": "stage",
+                        "interactionType": "videoAudio",
+                        "role": "-1",
+                        "aiSticker": "-1"
+                    }
+                ]
+            },
+            "skillHotArea": {
+                "count": 1,
+                "operationType": "All",
+                "location": [
+                    {
+                        "id": "bf7fb85e-b165-49f3-bb0b-1449f0345cd2",
+                        "index": 1,
+                        "name": "技能热区_1",
+                        "hotAreaType": "skill",
+                        "role": "-1",
+                        "roleSkill": "-1",
+                        "skillMaterialId": "-1",
+                        "imageId": "-1"
+                    }
+                ]
+            },
+            "partInteractionInfo": {
+                "playRule": "voiceRecognition",
+                "voiceRecognition": {
+                    "isRoleCheck": 1,
+                    "role": "classAssistant",
+                    "keyword": "我,小助教"
+                }
+            },
+            "savePending": {
+                "studentHotArea": {
+                    "location": [
+                        {
+                            "role": "-1",
+                            "aiSticker": "-1"
+                        },
+                        {
+                            "role": "-1",
+                            "aiSticker": "-1"
+                        },
+                        {
+                            "role": "-1",
+                            "aiSticker": "-1"
+                        },
+                        {
+                            "role": "-1",
+                            "aiSticker": "-1"
+                        },
+                        {
+                            "role": "-1",
+                            "aiSticker": "-1"
+                        },
+                        {
+                            "role": "-1",
+                            "aiSticker": "-1"
+                        }
+                    ]
+                },
+                "stageHotArea": {
+                    "location": [
+                        {
+                            "role": "-1",
+                            "aiSticker": "-1"
+                        }
+                    ]
+                },
+                "skillHotArea": {
+                    "location": [
+                        {
+                            "role": "-1",
+                            "roleSkill": "-1",
+                            "skillMaterialId": "-1",
+                            "imageId": "-1"
+                        }
+                    ]
+                }
+            }
+        },
+        "coursewareConfig": {
+            "location": [
+                {
+                    "id": "f4389c0b-cb45-4d3f-a40d-8722f62147c0",
+                    "index": 1,
+                    "name": "学生热区_1",
+                    "hotAreaType": "student",
+                    "interactionType": "videoAudio",
+                    "role": "-1",
+                    "aiSticker": "-1"
+                },
+                {
+                    "id": "23fcc53e-6a94-456d-ae19-839501f24a2b",
+                    "index": 2,
+                    "name": "学生热区_2",
+                    "hotAreaType": "student",
+                    "interactionType": "videoAudio",
+                    "role": "-1",
+                    "aiSticker": "-1"
+                },
+                {
+                    "id": "094c9d88-a46a-4b75-aefb-5a574a9fc786",
+                    "index": 3,
+                    "name": "学生热区_3",
+                    "hotAreaType": "student",
+                    "interactionType": "videoAudio",
+                    "role": "-1",
+                    "aiSticker": "-1"
+                },
+                {
+                    "id": "12b2449d-72ae-4938-8de9-7cb9b912cf8f",
+                    "index": 4,
+                    "name": "学生热区_4",
+                    "hotAreaType": "student",
+                    "interactionType": "videoAudio",
+                    "role": "-1",
+                    "aiSticker": "-1"
+                },
+                {
+                    "id": "ac482442-3f12-40b4-b28d-b76a30f34303",
+                    "index": 5,
+                    "name": "学生热区_5",
+                    "hotAreaType": "student",
+                    "interactionType": "videoAudio",
+                    "role": "-1",
+                    "aiSticker": "-1"
+                },
+                {
+                    "id": "10041042-967e-4151-bf78-6240e597c9f3",
+                    "index": 6,
+                    "name": "学生热区_6",
+                    "hotAreaType": "student",
+                    "interactionType": "videoAudio",
+                    "role": "-1",
+                    "aiSticker": "-1"
+                },
+                {
+                    "id": "8510659f-a677-4bc6-9d90-aa473e64b7a3",
+                    "index": 1,
+                    "name": "上台热区_1",
+                    "hotAreaType": "stage",
+                    "interactionType": "videoAudio",
+                    "role": "-1",
+                    "aiSticker": "-1"
+                },
+                {
+                    "id": "bf7fb85e-b165-49f3-bb0b-1449f0345cd2",
+                    "index": 1,
+                    "name": "技能热区_1",
+                    "hotAreaType": "skill",
+                    "role": "-1",
+                    "roleSkill": "-1",
+                    "skillMaterialId": "-1",
+                    "imageId": "-1"
+                }
+            ],
+            "canSave": 0,
+            "message": "请把公共配置中的待填选项改为有效选项！"
+        }
+    }
+}

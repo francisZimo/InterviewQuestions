16ec995b16230e17bf7f36aecd6731298350a672
franciszimo <wangyongju@tal.com>
2024-11-18 15:06:16 +0800

    feat: remove company code

--- 调试/node 调试/node1/index.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs/promises");
-
-(async function () {
-  const fileContent = await fs.readFile("./package.json", {
-    encoding: "utf-8"
-  });
-
-  await fs.writeFile("./package2.json", fileContent);
-})();
